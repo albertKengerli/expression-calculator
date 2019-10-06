a356b7b2e52c4aa9d038a5a72dd4afeb8975b457
albertKengerli <50487338+albertKengerli@users.noreply.github.com>
2019-10-07 01:50:36 +0300

    fix: readjust parsing of negative numbers

--- src/index.js
+++ src/index.js
@@ -7,24 +7,55 @@ function expressionCalculator(expr) {
 
   expr = expr.replace(/\s+/g, '');
 
+  function unbracket(string) {
+  	//console.log(string, "THIS WE WILL UNBRACKET");
+    let openPos = string.lastIndexOf("(");
+    let closePos = string.indexOf(")");
+
+    if ((closePos != -1 && openPos === -1) || (closePos === -1 && openPos != -1)) {
+      throw "ExpressionError: Brackets must be paired";
+    }
+
+    let currentBracket = add(string.substring(openPos+1, closePos));
+		//console.log("RESULT OF UNBR: ",currentBracket);
+    currentBracket = currentBracket.toString();
+    currentBracket = currentBracket.replace("-","neg");
+
+    let result = string.substring(0,openPos)+currentBracket+string.substring(closePos+1);
+    //console.log("UNBRACKETED", result);
+
+    return result;
+  }
+
   function add(string) {
+  	//console.log(string, "THIS WE WILL ADD");
     let currentArray = string.split("+");
     currentArray = currentArray.map(substract);
-    return currentArray.reduce((a,b) => a+b);
+    return currentArray.reduce((a,b) =>  {
+    	//console.log("add", a,b, "result:", a+b);
+      return a + b});
   }
 
   function substract(string) {
+  	//console.log(string, "THIS WE WILL SUBSTRACT");
     let currentArray = string.split("-");
-    currentArray = currentArray.map(multiply);
-    return currentArray.reduce((a,b) => a-b);
+    //console.log("LETS ROCK", currentArray);
+    currentArray = currentArray.map(divide);
+    return currentArray.reduce((a,b) => {
+    	//console.log("substract", a,b, "result:", a-b);
+      return a - b});
   }
 
   function divide(string) {
+  	//console.log(string, "THIS WE WILL DIVIDE");
     let currentArray = string.split("/");
-    currentArray = currentArray.map(Number);
+    ////console.log(currentArray);
+    currentArray = currentArray.map(multiply);
+    ////console.log(currentArray);
     return currentArray.reduce((a,b) => {
+      //console.log("divide", a,b, "result:", a/b);
       if (b === 0) {
-        throw "TypeError: Division by zero."
+        throw "TypeError: Division by zero.";
       } else {
         return a / b;
       }
@@ -32,33 +63,25 @@ function expressionCalculator(expr) {
   }
 
   function multiply(string) {
+  	//console.log(string, "THIS WE WILL MULTIPLY");
     let currentArray = string.split("*");
-    currentArray = currentArray.map(divide);
-    return currentArray.reduce((a,b) => a*b);
+
+    currentArray = currentArray.map((currentNumber) => {
+    	currentNumber = currentNumber.replace("neg","-")
+      return Number(currentNumber);
+    });
+
+    return currentArray.reduce((a,b) => {
+    	//console.log("multiply", a,b, "result:", a*b);
+      return a * b});
   }
 
 
-  //  return (add(expr));
-  // this will only take strings containing * operator [ cur + ]
-// const parseMultiplicationSeparatedExpression = (expression) => {
-// 	const numbersString = expression.split('*');
-// 	const numbers = numbersString.map(element => {
-//     return +element;
-//   });
+  while (expr.indexOf("(") != -1 || expr.indexOf(")") != -1) {
+  	expr=unbracket(expr);
+  }
 
-// 	const result = numbers.reduce((acc, cur) => acc * cur);
-// 	return result;
-// };
-
-// // * +
-// function add (expression){
-// 	const numbersString = expression.split('+');
-// 	const numbers = numbersString.map(parseMultiplicationSeparatedExpression);
-// 	const result = numbers.reduce((first,next) => first + next);
-// 	return result;
-// };
-
-  return add(expr);
+  return (add(expr));
 
 }
 
