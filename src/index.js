4a5375d3710ea9b3d1190a26b39aad1de1b5d308
albertKengerli <50487338+albertKengerli@users.noreply.github.com>
2019-10-07 23:36:25 +0300

    feat: add brackets and nested brackets support

--- src/index.js
+++ src/index.js
@@ -7,53 +7,64 @@ function expressionCalculator(expr) {
 
   expr = expr.replace(/\s+/g, '');
 
+
+
   function unbracket(string) {
-  	//console.log(string, "THIS WE WILL UNBRACKET");
+  	//consle.log(string, "THIS WE WILL UNBRACKET");
+    // if (expr.match(/\(/g).length != expr.match(/\)/g).length) {
+    //   throw "ExpressionError: Brackets must be paired";
+    // }
+
     let openPos = string.lastIndexOf("(");
-    let closePos = string.indexOf(")");
+    let closePos = string.indexOf(")",openPos);
 
     if ((closePos != -1 && openPos === -1) || (closePos === -1 && openPos != -1)) {
       throw "ExpressionError: Brackets must be paired";
     }
 
     let currentBracket = add(string.substring(openPos+1, closePos));
-		//console.log("RESULT OF UNBR: ",currentBracket);
+		//consle.log("RESULT OF UNBR: ",currentBracket);
     currentBracket = currentBracket.toString();
     currentBracket = currentBracket.replace("-","neg");
 
     let result = string.substring(0,openPos)+currentBracket+string.substring(closePos+1);
-    //console.log("UNBRACKETED", result);
+    //consle.log("UNBRACKETED", result);
 
     return result;
   }
 
   function add(string) {
-  	//console.log(string, "THIS WE WILL ADD");
+  	//consle.log(string, "THIS WE WILL ADD");
     let currentArray = string.split("+");
     currentArray = currentArray.map(substract);
     return currentArray.reduce((a,b) =>  {
-    	//console.log("add", a,b, "result:", a+b);
+    	//consle.log("add", a,b, "result:", a+b);
       return a + b});
   }
 
   function substract(string) {
-  	//console.log(string, "THIS WE WILL SUBSTRACT");
+  	//consle.log(string, "THIS WE WILL SUBSTRACT");
     let currentArray = string.split("-");
-    //console.log("LETS ROCK", currentArray);
-    currentArray = currentArray.map(divide);
+    //consle.log("LETS ROCK", currentArray);
+    currentArray = currentArray.map(multiply);
     return currentArray.reduce((a,b) => {
-    	//console.log("substract", a,b, "result:", a-b);
+    	//consle.log("substract", a,b, "result:", a-b);
       return a - b});
   }
 
   function divide(string) {
-  	//console.log(string, "THIS WE WILL DIVIDE");
+  	//consle.log(string, "THIS WE WILL DIVIDE");
     let currentArray = string.split("/");
-    ////console.log(currentArray);
-    currentArray = currentArray.map(multiply);
-    ////console.log(currentArray);
+    //consle.log(currentArray);
+    //consle.log(currentArray);
+
+    currentArray = currentArray.map((currentNumber) => {
+      currentNumber = currentNumber.replace("neg","-")
+      return Number(currentNumber);
+    });
+
     return currentArray.reduce((a,b) => {
-      //console.log("divide", a,b, "result:", a/b);
+      //consle.log("divide", a,b, "result:", a/b);
       if (b === 0) {
         throw "TypeError: Division by zero.";
       } else {
@@ -63,21 +74,18 @@ function expressionCalculator(expr) {
   }
 
   function multiply(string) {
-  	//console.log(string, "THIS WE WILL MULTIPLY");
+    //consle.log(string, "THIS WE WILL MULTIPLY");
     let currentArray = string.split("*");
 
-    currentArray = currentArray.map((currentNumber) => {
-    	currentNumber = currentNumber.replace("neg","-")
-      return Number(currentNumber);
-    });
+    currentArray = currentArray.map(divide);
 
     return currentArray.reduce((a,b) => {
-    	//console.log("multiply", a,b, "result:", a*b);
+    	//consle.log("multiply", a,b, "result:", a*b);
       return a * b});
   }
 
 
-  while (expr.indexOf("(") != -1 || expr.indexOf(")") != -1) {
+  while ((expr.indexOf("(") != -1) || (expr.indexOf(")") != -1)) {
   	expr=unbracket(expr);
   }
 
